3b54f30782a9a67d3778dc110710026780e28211
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-13 03:22:20 +0330

    create servicePayment & sumAlltotatl

--- Q10/js/app.js
+++ Q10/js/app.js
@@ -1,39 +1,58 @@
 "use strict";
 const plus = document.querySelectorAll(".plus");
 const minus = document.querySelectorAll(".minus");
+const sumAllOrder = document.getElementById("sum-All-Order");
+const servicePrice = document.getElementById("service-price");
+
 [...plus].forEach((item) => {
-  item.addEventListener("click", plusClick);
+    item.addEventListener("click", plusClick);
 });
 [...minus].forEach((item) => {
-  item.addEventListener("click", minusClick);
+    item.addEventListener("click", minusClick);
 });
 
 function plusClick(e) {
-  let spanCount = e.currentTarget.nextSibling.nextSibling.childNodes[1];
-  spanCount.innerText = +spanCount.innerText + 1;
-  const priceMain =
+    let spanCount = e.currentTarget.nextSibling.nextSibling.childNodes[1];
+    spanCount.innerText = +spanCount.innerText + 1;
+    const priceMain =
     e.currentTarget.parentElement.previousSibling.previousSibling.childNodes[3]
-      .childNodes[0].innerText;
-  const priceTotal =
+    .childNodes[0].innerText;
+    const priceTotal =
     e.currentTarget.parentElement.parentElement.nextSibling.nextSibling
-      .childNodes[1].childNodes[0];
-
-  priceTotal.innerHTML = spanCount.innerHTML * priceMain;
+    .childNodes[1].childNodes[0];
+    
+    const priceCard = (priceTotal.innerHTML = spanCount.innerHTML * priceMain);
+    totalPrice();
+}
+let a = [];
+function minusClick(e) {
+    const spanCount =
+    e.currentTarget.previousSibling.previousSibling.childNodes[1];
+    // console.log(spanCount);
+    if (spanCount.innerText > 0) {
+        spanCount.innerText = spanCount.innerText - 1;
+    }
+    
+    const priceMain =
+    e.currentTarget.parentElement.previousSibling.previousSibling.childNodes[3]
+    .childNodes[0].innerText;
+    const priceTotal =
+    e.currentTarget.parentElement.parentElement.nextSibling.nextSibling
+    .childNodes[1].childNodes[0];
+    const priceCard = (priceTotal.innerHTML = spanCount.innerHTML * priceMain);
+    totalPrice();
 }
 
-function minusClick(e) {
-  const spanCount =
-    e.currentTarget.previousSibling.previousSibling.childNodes[1];
-  // console.log(spanCount);
-  if (spanCount.innerText > 0) {
-    spanCount.innerText = spanCount.innerText - 1;
-  }
+function totalPrice() {
+    const card = document.getElementById("card");
+    const eachPrice = card.querySelectorAll(".totalPrice");
+    const reducePrices =  [...eachPrice].reduce((a, b) => {
+        return +a.innerHTML + +b.innerHTML;
+    });
+    sumAllOrder.innerText = reducePrices;
+    servicePayment(sumAllOrder.innerText)
+}
 
-  const priceMain =
-    e.currentTarget.parentElement.previousSibling.previousSibling.childNodes[3]
-      .childNodes[0].innerText;
-  const priceTotal =
-    e.currentTarget.parentElement.parentElement.nextSibling.nextSibling
-      .childNodes[1].childNodes[0];
-  priceTotal.innerHTML = spanCount.innerHTML * priceMain;
+function servicePayment(params) {
+    servicePrice.innerText = (params*50)/1000
 }
